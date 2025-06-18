8c600918181fab2b244d33279e39a9472bba7ed1
malikharispk <67654098+malikharispk@users.noreply.github.com>
2025-06-18 23:56:14 +0800

    visualizer.py

---
+++ utils/visualizer.py
@@ -0,0 +1,7 @@
+import open3d as o3d
+import numpy as np
+
+def visualize_point_cloud(points):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
+    o3d.visualization.draw_geometries([pcd])
